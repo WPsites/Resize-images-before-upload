e505e1196f6db4b05bd9373d38674c07ef4baef4
Simon D <simon@wpsites.co.uk>
2012-08-14 16:05:18 +0100

    Upload 0.6

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://wordpress.org/extend/plugins/resize-images-before-upload/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/resize-images-before-upload/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="WPsites" # your svn username
 SVNIGNORE="
 backups
